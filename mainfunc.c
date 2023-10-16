1d92cb5bf2432e957605646b429948c58f9173a9
Luke Herbst <lukeherbst37@gmail.com>
2023-10-15 19:15:23 -0700

    added checks for format type of 'c' and 's'

--- mainfunc.c
+++ mainfunc.c
@@ -12,22 +12,33 @@ int _printf(const char *format, ...)
 	va_list printer;
 	int i;
 	int num_args;
+	char letter;
 	char *string_arg;
 
 	num_args == 0;
 	i = 0;
 	while (format[i] != '\0')
 	{
-		if (format[i] == '%')
+		if (format[i] == '%') /* this counters format specifiers so I can find out the num of args passed in, but I must exit the program if too little args are passed with to many format specifiers? */
 			num_args++;
 		i++;
 	}
+	va_start(printer, num_args); /* last defined parameter is format but surely number of args should be passed here? */
 	j = 0;
 	for (i = 0; format[i] != '\0', i++) /* this will pass args into a new loop j everytime it comes across a type specifier */
 	{
 		if (format[i] == '%')
 		{
-			string_arg = &(type_handler(format[i + 1]));
+			if (format[i + 1] == 'c')
+				string_arg = &(va_arg(printer, char));
+			if (format[i + 1] == 's')
+				string_arg = &(va_arg(printer, char *));
+			for (j = 0; j < sizeof(string_arg); j++)
+			{
+				putchar(string_arg[0]);
+			}
 		}
+		if (format[i] == '\n')
+			putchar('\n');
 	}
 }
