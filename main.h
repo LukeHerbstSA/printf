3535495fdec0777396eea44a6c5367310465eeb2
Luke Herbst <lukeherbst37@gmail.com>
2023-10-16 20:26:18 -0700

    added type handler function and added conditions to evaluate special chars

--- main.h
+++ main.h
@@ -2,5 +2,10 @@
 #define MAIN_H
 
 int _printf(const char *format, ...);
+typedef struct types_funcs
+{
+	char *type;
+	char *()()
+}
 
 #endif
--- mainfunc.c
+++ mainfunc.c
@@ -19,26 +19,33 @@ int _printf(const char *format, ...)
 	i = 0;
 	while (format[i] != '\0')
 	{
-		if (format[i] == '%') /* this counters format specifiers so I can find out the num of args passed in, but I must exit the program if too little args are passed with to many format specifiers? */
+		if (format[i] == '%') /* this counts format specifiers so I can find out the num of args passed in, but I must exit the program if too little args are passed with too many format specifiers? */
 			num_args++;
 		i++;
 	}
-	va_start(printer, num_args); /* last defined parameter is format but surely number of args should be passed here? */
+	va_start(printer, format); /* last defined parameter is format but surely number of args should be passed here? va_start might have to take *format as a second arg*/
 	j = 0;
 	for (i = 0; format[i] != '\0', i++) /* this will pass args into a new loop j everytime it comes across a type specifier */
 	{
 		if (format[i] == '%')
 		{
 			if (format[i + 1] == 'c')
-				string_arg = &(va_arg(printer, char));
+				string_arg = &(va_arg(printer, int)); /* issue is here how to get the 2nd parameter passed */
 			if (format[i + 1] == 's')
 				string_arg = &(va_arg(printer, char *));
 			for (j = 0; j < sizeof(string_arg); j++)
 			{
-				putchar(string_arg[0]);
+				putchar(string_arg[j]);
 			}
 		}
-		if (format[i] == '\n')
-			putchar('\n');
+		else if (format[i] == '\')
+			
+		else
+			putchar(format[i]);
 	}
+}
+
+char *type_handler(*type)
+{
+	
 }
