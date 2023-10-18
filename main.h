bfa2d7ced4b1c25a2ed93932260c4757c57052c4
mohd33034914 <mohd33034914@gmail.com>
2023-10-18 15:39:58 -0700

    printf

--- main.h
+++ main.h
@@ -1,24 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdarg.h>
 
-#include <limits.h>
-#include <stdio.h>
-#include "main.h"
-
-/**
-	* struct type_ptr - used to access ptr for relevant function for that format
-	* @type: passed char that will act as key to access func_ptr
-	* @func_ptr: func ptr to any one of the functions in type_handlers.c
-	*/
-
-typedef struct type_ptr
-{
-	char type;
-	int (*func_ptr)(va_list printer);
-} func_ptrs;
-int c_handler(va_list printer);
-int s_handler(va_list printer);
+int _putchar(char c);
 int _printf(const char *format, ...);
-int type_chooser(char type, va_list printer);
+int _print_char(va_list args);
+int _print_int(va_list args);
+int _print_str(va_list args);
+int (*_selec_func(char c))(va_list);
 
 #endif
