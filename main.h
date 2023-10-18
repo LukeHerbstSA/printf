bdcc0acfeafb30c31f38ff6457064d7dabe94dc4
mohd33034914 <mohd33034914@gmail.com>
2023-10-18 12:38:14 -0700

    printf

--- main.h
+++ main.h
@@ -1,14 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdarg.h>
-#include <stdio.h>
-#include <string.h>
 
 /**
 	* struct type_ptr - used to access ptr for relevant function for that format
 	* @type: passed char that will act as key to access func_ptr
 	* @func_ptr: func ptr to any one of the functions in type_handlers.c
 	*/
+
 typedef struct type_ptr
 {
 	char type;
