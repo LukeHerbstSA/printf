6d4d5074c11ea43c6cdbafa414fcdd4727051813
System Administrator <root@Lukes-MacBook-Air.local>
2023-10-18 12:11:56 +0200

    various betty fixes, also now need to fix undefined reference error for type_handlers.c functions

--- main.h
+++ main.h
@@ -5,14 +5,14 @@
 #include <string.h>
 
 /**
-        * struct type_ptr - used to access ptr for relevant function for that format
-        * @type: passed char that will act as key to access func_ptr
-        * @func_ptr: func ptr to any one of the functions in type_handlers.c
-        */
+	* struct type_ptr - used to access ptr for relevant function for that format
+	* @type: passed char that will act as key to access func_ptr
+	* @func_ptr: func ptr to any one of the functions in type_handlers.c
+	*/
 typedef struct type_ptr
 {
-        char type;
-        int (*func_ptr)(va_list printer);
+	char type;
+	int (*func_ptr)(va_list printer);
 } func_ptrs;
 int _printf(const char *format, ...);
 int type_chooser(char type, va_list printer);
